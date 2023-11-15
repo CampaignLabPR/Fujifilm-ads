c87123ae854b2aad04c127c950063f79fde0bcaa
Jeremie SIGUAN <150916716+CampaignLabPR@users.noreply.github.com>
2023-11-15 22:55:11 +1100

    First Commit

---
+++ setup.js
@@ -0,0 +1,5 @@
+//00-300x250-HOP-CAPTURE-A
+const pre       = "00-";
+const sizeList  = ['300x250', '160x600'];
+const animList  = ['HOP-CAPTURE', 'HOP-GALLERY', 'HOP-PRINT', 'HOP-QUIZ', 'HOP-STUDIO'];
+const version   = ['A','B'];
